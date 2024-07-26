880d446714dfe76ccb0d7ac4838b3a81022339c1
Israel <mtzdgtlab@gmail.com>
2024-07-26 02:49:18 -0400

    coming_simple

---
+++ script.js
@@ -0,0 +1,20 @@
+// Countdown Timer
+const countdown = () => {
+    const countDate = new Date('August 1, 2024 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector('.countdown').innerText = `${textDay} DAYS ${textHour}:${textMinute}:${textSecond}`;
+};
+
+setInterval(countdown, 1000);
